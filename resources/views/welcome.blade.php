026d9ddd9ebbc83bb67d01384805b786d137827c
Koen-Smit <93587490+Koen-Smit@users.noreply.github.com>
2022-11-13 22:28:49 +0100

    text and auth changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,25 +1,25 @@
 @extends('layouts.base')
 
 @section('content')
-  <h1>Hello homepage!</h1>
+  <h1 class="display-3">Home</h1>
+  @if (auth()->user())
+  <p></p>
+  @else
+  <p>U moet een account maken of inloggen om van de site gebruik te kunnen maken.</p>
+  @endif
   
-
   @auth
-    <p>Welkom {{ auth()->user()->name }}</p>
-  <h2>Uw orders</h2>
-  <ul class="list-group">
+    <p class="lead mt-1">Welkom {{ auth()->user()->name }}</p>
+    <p>u bent ingelogd!</p>
+  <h2 class="mt-5">Uw orders:</h2>
+  <ul class="list-group mt-2">
     @foreach(\Auth::user()->orders as $order)
-      <a class="text-decoration-none" href="{{ route('tickets.scan', $order->id) }}">
+      <a class="text-decoration-none" href="{{ route('orders.confirm', $order->id) }}">
         <li class="list-group-item">
             {{ $order->id }} - gedaan op: {{$order->created_at}}
         </li>
       </a>
     @endforeach
   </ul>
-
-
-
-
-    <p>u bent ingelogd!</p>
   @endauth
 @endsection
