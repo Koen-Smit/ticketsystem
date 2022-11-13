138c132fa41f2f89b7f9ce6cbf189ab3535760c7
Koen-Smit <93587490+Koen-Smit@users.noreply.github.com>
2022-11-13 22:32:18 +0100

    removed scan functions, added confirm function

--- app/Http/Controllers/TicketsController.php
+++ app/Http/Controllers/TicketsController.php
@@ -2,24 +2,16 @@
 
 namespace App\Http\Controllers;
 use App\Models\Ticket;
+use App\Models\Order;
 
 use Illuminate\Http\Request;
 
 class Ticketscontroller extends Controller
 {
-    public function scan($ticket_id) {
-        $ticket = Ticket::findOrFail($ticket_id);
-        $scanned = false;
-        if(!$ticket->scanned_at) {
-            $ticket->scanned_at = now();
-            $ticket->save();
-            $scanned = true;
-        }
-        return view('ticket-scanned', [
-            'ticket' => $ticket
+    public function confirm($ticket_id) {
+        $order = order::findOrFail($ticket_id);
+        return view('orders.confirm', [
+            'order' => $order,  
         ]);
-    }
-    public function scanView() {
-        return view('scan');
     }
 }
