819f9b31836e03244521b376a85565cc037913c1
Koen-Smit <93587490+Koen-Smit@users.noreply.github.com>
2022-11-13 22:27:27 +0100

    Styling changes to index

--- resources/views/events/index.blade.php
+++ resources/views/events/index.blade.php
@@ -4,12 +4,14 @@
 @if(session('message'))
     {{ session('message') }}
 @endif
-<h1 class="display-1">Events</h1>
+<h1 class="display-3">Events</h1>
+<p>- Klik op de titel om tickets te bestellen.</p>
+<p>- Deleten werkt niet als er al tickets bij dat evenement besteld zijn.</p>
 <ul>
   <table class="table">
     <thead>
       <tr>
-        <th scope="col">Title</th>
+        <th scope="col ">Title</th>
         <th scope="col">description</th>
         <th scope="col">Start</th>
         <th scope="col">End</th>
@@ -25,7 +27,7 @@
     <tbody>
       @foreach ($events as $event)
         <tr>
-          <td><a href="{{route('events.show', $event->id)}}">{{$event->title}}</a></td>
+          <td><a class="text-danger" href="{{route('events.show', $event->id)}}">{{$event->title}}</a></td>
           <td>{{ $event->description}}</td>
           <td>{{ $event->start_date}}</td>
           <td>{{ $event->end_date}}</td>
@@ -40,7 +42,7 @@
             <form action="{{route('events.destroy', $event)}}" method="POST">
               @csrf
               @method('DELETE')
-              <button type="submit" class="btn btn-danger">Delete</button>
+              <button class="btn btn-danger">Delete</button>
             </form>
           </td>
         </tr>
